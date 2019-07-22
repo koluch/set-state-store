e95a21a9640dcec6cd914c87ef30559026d74443
Nikolai Mavrenkov <koluch@koluch.ru>
2019-07-22 18:34:49 +0300

    Use Flow for typing

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
-export function createStore(initialState) {
-  let state = initialState;
-  let observers = [];
+//@flow strict
+export type TObserver<T> = (state: T, prevState: T) => void;
+
+export type TStore<T: {}, PT: $Shape<T>> = {
+  getState: () => T,
+  setState: (diff: PT) => void,
+  subscribe: (observer: TObserver<T>) => void,
+  unsubscribe: (observer: TObserver<T>) => void
+};
+
+export function createStore<T: {}, PT: $Shape<T>>(
+  initialState: T
+): TStore<T, PT> {
+  let state: T = initialState;
+  let observers: TObserver<T>[] = [];
   return {
-    setState: diff => {
-      let prevState = state;
+    setState: (diff: PT) => {
+      let prevState: T = state;
       state = Object.assign({}, state, diff);
       for (let i = 0; i < observers.length; i += 1) {
         const observer = observers[i];
         observer(state, prevState);
       }
     },
-    getState: () => {
+    getState: (): T => {
       return state;
     },
-    subscribe: observer => {
+    subscribe: (observer: TObserver<T>) => {
       observers.push(observer);
     },
-    unsubscribe: observer => {
-      observers = observers.filter(function(x) {
-        return x !== observer;
-      });
+    unsubscribe: (observer: TObserver<T>) => {
+      observers = observers.filter(x => x !== observer);
     }
   };
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,4 @@
+//@flow strict
 import { createStore } from "./index.js";
 
 test("initial state propertly initialize store", () => {
@@ -6,28 +7,22 @@ test("initial state propertly initialize store", () => {
 });
 
 test("setState should properly save value", () => {
-  let store = createStore({});
+  let store = createStore({ a: 666 });
   store.setState({ a: 42 });
   expect(store.getState()).toEqual({ a: 42 });
 });
 
 test("setState merges passed diff", () => {
-  let store = createStore({ a: 42 });
-  store.setState({ b: 666 });
-  expect(store.getState()).toEqual({ a: 42, b: 666 });
-});
-
-test("setState shallow merges passed diff", () => {
-  let store = createStore({ a: 42, b: { c: 777 } });
+  let store = createStore({ a: 42, b: 0 });
   store.setState({ b: 666 });
   expect(store.getState()).toEqual({ a: 42, b: 666 });
 });
 
 test("observed called after setState with new and old state", () => {
-  let store = createStore({ a: 42 });
+  let store = createStore({ a: 42, b: 0 });
   store.subscribe((newState, oldState) => {
     expect(newState).toEqual({ a: 42, b: 666 });
-    expect(oldState).toEqual({ a: 42 });
+    expect(oldState).toEqual({ a: 42, b: 0 });
   });
   store.setState({ b: 666 });
 });
